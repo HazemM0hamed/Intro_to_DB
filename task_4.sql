588d3cbdac9b6c36e51663f94fb555b117f6d5b8
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-07-28 23:11:34 +0300

    Full description

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- Use the specified database
+USE alx_book_store;
+
+-- Select full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() AND 
+    TABLE_NAME = 'books';
+
